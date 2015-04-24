e12c42a7258f090bdc1b3af7983a95feca9424ce
Soren Peterson <soren468@gmail.com>
2015-04-24 18:12:42 -0500

    Visitor count implemented

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
+  before_filter :log_visitor
+
   def index
   end
 
   def info
+    @visitor_count = Visitor.count
+  end
+
+  def log_visitor
+    Visitor.find_or_create_by(session_id: session['session_id'])
   end
 end
