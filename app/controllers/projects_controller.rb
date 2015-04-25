0fde78c070a0880ad5371d81fc1890067b4a5af9
Soren Peterson <soren468@gmail.com>
2015-04-25 00:03:31 -0500

    Can upload picture for a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,12 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    name = params[:project][:image].original_filename
+    directory = "public/img/projects"
+    path = File.join(directory, name)
+    File.open(path, "wb") { |f| f.write(params[:project][:image].read) }
+    # redirect_to "/projects/new"
+    redirect_to '/img/projects/' + name
   end
 
   def update
