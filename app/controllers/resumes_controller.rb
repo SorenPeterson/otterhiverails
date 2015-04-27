96aaabdf004605ad3d8f51675ae464f0eb2cd24f
Soren Peterson <soren468@gmail.com>
2015-04-27 11:36:44 -0500

    Fix redirect loop in resume controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,10 +1,8 @@
 class ResumesController < ApplicationController
   http_basic_authenticate_with name: 'otter', password: 'hive'
+  skip_before_filter :require_admin
 
   def index
     render layout: false
-  end
-
-  def is_admin?
   end
 end
