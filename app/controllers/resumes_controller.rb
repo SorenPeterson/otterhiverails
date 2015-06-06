c57a77def7510b556687da6d057c5334a9536ef4
Soren Peterson <soren468@gmail.com>
2015-06-06 09:13:04 -0700

    add special link to resume for easy access

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,12 @@
 class ResumesController < ApplicationController
-  http_basic_authenticate_with name: 'otter', password: 'hive', only: ['index']
-  skip_before_filter :require_admin
+	http_basic_authenticate_with name: 'otter', password: 'hive', only: ['index']
+	skip_before_filter :require_admin
 
-  def index
-    render layout: false
-  end
+	def index
+		render layout: false
+	end
+
+	def doge
+		redirect_to :index unless params['doge'] = 'abcdef'
+	end
 end
