590ddfd9028387283d2ef34cbaafd013e0524836
Soren Peterson <soren468@gmail.com>
2015-06-06 09:01:50 -0700

    Expose secrets, only authenticate resume index

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,5 @@
 class ResumesController < ApplicationController
-  http_basic_authenticate_with name: 'otter', password: 'hive'
+  http_basic_authenticate_with name: 'otter', password: 'hive', only: ['index']
   skip_before_filter :require_admin
 
   def index
